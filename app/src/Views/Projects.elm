1c27160f5e7e397518417d33d48384ba376cd427
Gery Debongnie <gery.debongnie@gmail.com>
2015-10-17 22:47:37 +0200

    add some structure to the project view

--- app/src/Views/Projects.elm
+++ app/src/Views/Projects.elm
@@ -1,17 +1,46 @@
 module Views.Projects where
 
-import Html exposing (Html, text, h1, div, p)
+import Html exposing (Html, text, h1, div, p, h2)
 import UI
 
 view: UI.View
 view = UI.genericView "Projects" content
 
+projects : List Project
+projects =
+    [ moebius ]
 
--- projects
+
 content: List Html
 content =
-    [ h1 [] [text "Projects"]
-    , p [] [ text "content" ]
-    ]
+    [ h1 [] [text "Projects"] ] ++ (List.concatMap renderProject projects)
 
 
+type alias Project =
+    { name: String
+    , repo: String
+    , description: List String
+    }
+
+renderProject: Project -> List Html
+renderProject project =
+    let
+        title =
+            h1 [] [ text project.name ]
+        subtitle =
+            h2 [] [ text ("Repository: " ++ project.repo) ]
+        content =
+            List.map (\paragraph -> p [] [ text paragraph ]) project.description
+    in
+        [title, subtitle] ++ content
+
+-- projects
+moebius: Project
+moebius =
+    { name = "Moebius"
+    , repo = "https://github.com/gdeb/moebius"
+    , description =
+        [ "This is an experiment in a single page application. The code of this website is actually the javascript compiled by this project."
+        , "I'll develop more later on why this is interesting"
+        ]
+    }
