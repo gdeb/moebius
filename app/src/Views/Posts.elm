ce6218aeb4c7bf3b94abc9038cbd8771d30e6b28
Gery Debongnie <gery.debongnie@gmail.com>
2015-10-19 13:24:04 +0200

    flesh out the posts page, somewhat

--- app/src/Views/Posts.elm
+++ app/src/Views/Posts.elm
@@ -1,88 +1,58 @@
 module Views.Posts where
 
-import Html exposing (Html, text, h1, div)
+import Html exposing (Html, text, h1, h2, h3, div)
 import UI
+import Date exposing (Date)
+import Date.Format exposing (format)
 
 
 view: UI.View
 view = UI.genericView "Posts" content
 
+posts : List Post
+posts =
+    [ test1, test1, test1 ]
 
 
--- posts
 content: List Html
 content =
-    [ h1 [] [text "Posts"]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    , div [] [ text "aaaa" ]
-    ]
+    [ h1 [] [text "Posts"] ] ++ (List.concatMap renderPost posts)
 
+type alias Post =
+    { title: String
+    , subtitle: String
+    , date: Date
+    , summary: List String
+    , content: List Html
+    }
+
+renderPost: Post -> List Html
+renderPost post =
+    let
+        title =
+            h2 [] [text post.title]
+
+        subtitle =
+            h3 [] [ text post.subtitle ]
+
+        date =
+            div [] [ text (formatDate post.date) ]
+    in
+        [title, subtitle, date] ++ post.content
+
+-- posts
+
+test1 =
+    { title = "A test post"
+    , subtitle = "Small subtitle to highlight important stuff"
+    , date = unsafeReadDate "10/19/2016"
+    , summary = [ "summary. this is a paragraph", "and this is another"]
+    , content = [ div [] [text "main content"], div [] [text "other stuff"] ]
+    }
+
+formatDate = format "%B %e, %Y"
+
+unsafeReadDate : String -> Date
+unsafeReadDate value =
+    case Date.fromString value of
+       Ok date -> date
